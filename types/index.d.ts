792664ec0da2271428014e258f681406dd64bb70
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-07-24 19:15:35 -0300

    Fix lazy components type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,15 @@
+import { LazyExoticComponent } from "react";
+
+type LazyComponent = LazyExoticComponent<() => JSX.Element>
+type NormalComponent = () => JSX.Element
+
 export interface RouteObject {
     path : string;
-    component: () => JSX.Element;
+    component: NormalComponent | LazyComponent;
 }
+
 export declare function Link({href, target, ...props} : {href : string, target? : string, children?: React.ReactNode, className?: string}) : JSX.Element
 
-export declare function Route({path, component} : {path : string, component : () => JSX.Element}) : null
+export declare function Route({path, component} : {path : string, component : NormalComponent | LazyComponent}) : null
 
-export declare function Router({children, routes, defaultComponent} : {children? : React.ReactNode, routes? : Array<RouteObject>, defaultComponent : () => JSX.Element}) : JSX.Element
+export declare function Router({children, routes, defaultComponent} : {children? : React.ReactNode, routes? : Array<RouteObject>, defaultComponent : NormalComponent}) : JSX.Element
