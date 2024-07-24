fc28bcaa48480a0b1405040abd5cc4b1f44ba5b5
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-07-24 11:06:05 -0300

    Create route component

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import { match } from "path-to-regexp";
-import { useEffect, useState } from "react";
+import { Children, useEffect, useState } from "react";
 
 export function Router(routes = [], DefaultComponent){
     const [currentPath, setCurrentPath] = useState(window.location.pathname);
@@ -20,6 +20,11 @@ export function Router(routes = [], DefaultComponent){
 
     const routeParams = {};
 
+    const routesFromChildren = Children.map(children, ({props, type}) => {
+        const {name} = type;
+        return name === "Route" ? props : null;
+    })
+
     const Page = routes.find(({path}) =>{
         if(path === currentPath) return true;
 
@@ -30,6 +35,6 @@ export function Router(routes = [], DefaultComponent){
         routeParams = matched.params;
         return true;
     })?.Component;
-    
+
     return Page ? <Page routeParams={routeParams}/> : <DefaultComponent routeParams={routeParams}/>
 }
